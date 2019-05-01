98846ef02f45e385c379212cafcf25f505e16ff4
Carl0587 <Cam1la!9>
2019-04-30 23:01:00 -0500

    first upload

---
+++ src/components/NewStudentForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NewStudentForm',
+        data() {
+            return {
+                newStudentName: '',
+                newStarID: '',
+                errors: []
+            }
+        },
+        methods: {
+            addStudent() {
+                this.errors = []
+                if (this.newStudentName && this.newStarID) {
+                    let student = { name: this.newStudentName, starID: this.newStarID, present: false }
+                    this.$emit('student-added', student)
+                    this.newStudentName = ''
+                    this.newStarID = ''
+                } else {
+                    this.errors.push('Name and StarID are required.')
+                }
+            }
+        }
+    }
+
+</script>
+
+<style>
+    /* Write any styles for elements in this component here  */
+</style>
+
---
+++ src/components/StudentMessage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    /* Write any styles for elements in this component here  */
+</style>
+
